69196bb38da746d12b7c21dfbf52429b9db97dbd
aapool@outlook.com <aapool@outlook.com>
2016-04-12 03:31:01 +0800

    update

--- coinkit/test.py
+++ coinkit/test.py
@@ -1,10 +1,20 @@
 # -*- coding: cp936 -*-
 from coinkit.keypair import BitcoinKeypair
 class vb4(BitcoinKeypair):
-        _pubkeyhash_version_byte = 45
 
+    # _pubkeyhash_version_byte=10
 
-verb4 = vb4();
-print '��ַ:'+verb4.address()
-print '˽Կ��'+verb4.private_key()
-print '��Կ:\n'+verb4.public_key()
+    def __init__(self,pubkey_byte):
+        BitcoinKeypair._pubkeyhash_version_byte = pubkey_byte
+        BitcoinKeypair.__init__(self)
+
+verb4 = vb4(100);
+count = 0;
+while(verb4.address().upper().find('ZAO')<0):
+    count=count+1
+    print 'address    '+verb4.address()+'------'+str(count)+'  addressed searched!'
+    verb4 = vb4(80);
+
+print 'address    '+verb4.address()
+print 'priv key   '+verb4.private_key()
+print 'public_key '+verb4.public_key()
